ae5803fceddfc629c7e9b5b4e376282384d3cb50
rooty meow <job47@protonmail.ch>
2019-09-11 01:04:48 +0300

    Add jokes1.0.py

---
+++ jokes1.0.py
@@ -0,0 +1,27 @@
+#This is a program that tells jokes.
+
+
+print('What did the moon monster say when the rocket landed on the moon?')
+true_answer = 'yum, canned people!'
+print()
+
+print('Please enter your answer.')
+answer = input()
+# answer = input(answer)
+
+if answer == true_answer:
+    print('You got it right!')
+else:
+    print('The correct answer is '+ true_answer +'. Better luck in the next one!')
+
+print('What do you call an astronaut\'s cavity?')
+true_answerI = 'black hole'
+print()
+print('Please enter your answer.')
+answer = input()
+# answer = input(answer)
+
+if answer == true_answerI:
+    print('You got it right!')
+else:
+    print('The correct answer is '+ true_answerI +'. Better luck in the next one!')
